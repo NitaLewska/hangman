5f543e2ed013ff28771ab953fa85287e0b7f4185
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-12-12 03:08:55 +0300

    feat: add functionality to categories buttons (03:08:43 Tuesday, December 12, 2023)

---
+++ categories.js
@@ -0,0 +1,73 @@
+let categoriesButtons = document.querySelectorAll('.categories_button')
+Array.from(categoriesButtons).map((a, i) => a.addEventListener('click', changeCategory(i)))
+let menuCards = document.querySelectorAll('.menu_card')
+let coffeeCards = document.querySelectorAll('.menu_coffee')
+let teaCards = document.querySelectorAll('.menu_tea')
+let dessertCards = document.querySelectorAll('.menu_dessert')
+let loadButton = document.querySelector(".load_more")
+
+changeCategory(0)()
+
+function changeCategory(i) {
+    return function() {
+        console.log(i)
+        Array.from(categoriesButtons).map(a => a.classList.remove('active'))
+        Array.from(categoriesButtons)[i].classList.add('active')
+        Array.from(menuCards).map(a => a.classList.add('hidden'))
+        if(i === 0) {
+            Array.from(coffeeCards).map(a => a.classList.remove('hidden'))
+            if (window.screen.width < 769) {
+                Array.from(coffeeCards).slice(4).map(a => a.classList.add('hidden'))
+                        if(Array.from(coffeeCards).length < 5){
+            loadButton.classList.add('hidden')
+        } else {
+            loadButton.classList.remove('hidden')
+        }
+            }
+        } else if (i === 1) {
+            Array.from(teaCards).map(a => a.classList.remove('hidden'))
+            if (window.screen.width < 769) {
+                Array.from(teaCards).slice(4).map(a => a.classList.add('hidden'))
+            if(Array.from(teaCards).length < 5){
+            loadButton.classList.add('hidden')
+        } else {
+            loadButton.classList.remove('hidden')
+        }
+            }
+        } else {
+            Array.from(dessertCards).map(a => a.classList.remove('hidden'))
+            if (window.screen.width < 769) {
+                Array.from(dessertCards).slice(4).map(a => a.classList.add('hidden'))
+                        if(Array.from(dessertCards).length < 5){
+            loadButton.classList.add('hidden')
+        } else {
+            loadButton.classList.remove('hidden')
+        }
+            }
+        }
+    }
+}
+
+
+loadButton.addEventListener('click', loadMore)
+
+function loadMore() {
+    Array.from(menuCards).map(a => a.classList.add('hidden'))
+    let section = 0
+    if (Array.from(categoriesButtons)[1].classList.contains('active')) {
+        section = 1
+    }
+    if (Array.from(categoriesButtons)[2].classList.contains('active')) {
+        section = 2
+    }
+    if(section === 0) {
+        Array.from(coffeeCards).map(a => a.classList.remove('hidden'))
+    } else if (section === 1) {
+        Array.from(teaCards).map(a => a.classList.remove('hidden'))
+    } else if (section === 2){
+        Array.from(dessertCards).map(a => a.classList.remove('hidden'))
+    }
+    loadButton.classList.add('hidden')
+}
+
+alert("Извините, пожалуйста, если возможно - проверьте мою работу завтра вечером, доделываю модалку и слайдер (пишу в 3 утра 12.12)")
